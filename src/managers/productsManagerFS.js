0c81e34eebf5866767d5a5dd4aeb7aae2e722d72
arianaortigoza4 <arianaortigoza4@gmail.com>
2024-01-01 15:17:16 -0300

    Solucionado el id duplicado

--- src/managers/productsManagerFS.js
+++ src/managers/productsManagerFS.js
@@ -18,7 +18,7 @@ class ProductsManagerFS {
         try {
             const products = await this.readFile()
             let newProduct = {
-                id: products.length + 1,
+                id: products.length ? products[products.length-1].id + 1 : 1,
                 title: "",
                 description: "",
                 code: 0,
