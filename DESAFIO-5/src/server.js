e15a24b61cad3e475bdddc952f5caf13e58df3e8
arianaortigoza4 <arianaortigoza4@gmail.com>
2024-01-06 14:16:05 -0300

    conexion de cliente-servidor implementada

---
+++ DESAFIO-5/src/server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const cartsRouter = require('./routes/carts.router.js')
+const productsRouter = require('./routes/products.router.js')
+const handlebars  = require('express-handlebars')
+const { Server: ServerIO, Server }  = require('socket.io') 
+
+const app = express()
+
+console.log(__dirname+'/public')
+app.use(express.static(__dirname+'/public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname+'/views')
+app.set('view engine', 'handlebars')
+
+// http://localhost:8080 /
+app.get('/', (req,res)=>{
+    res.render('index', {} )
+})
+app.get('/realtimeproducts', (req,res)=>{
+    res.render('realtimeproducts', {} )
+})
+
+app.use('/api/carts',    cartsRouter)
+app.use('/api/products', productsRouter)
+
+
+const httpServer =  app.listen(8080, ()=>{
+    console.log('Escuchando en el puerto 8080')
+})
+// socket del lado del server
+const io = new ServerIO(httpServer)
+
+let mensajes = []
+
+function cbConnection(socket) {	
+    console.log('cliente conectado')
+    // io.emit('')
+    socket.on('message', data => {
+        console.log(data)
+        mensajes.push(data)
+
+        io.emit('messageLogs', mensajes)
+    })
+}
+
+io.on('connection', socket =>{cbConnection(socket)})
