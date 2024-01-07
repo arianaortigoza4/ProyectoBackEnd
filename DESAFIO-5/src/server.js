63321596b2ccd10b361b592b83a1cc2d72372d65
arianaortigoza4 <arianaortigoza4@gmail.com>
2024-01-07 17:07:28 -0300

    actualizacion de json en vista por polling

--- DESAFIO-5/src/server.js
+++ DESAFIO-5/src/server.js
@@ -28,6 +28,10 @@ app.use('/api/carts',    cartsRouter)
 app.use('/api/products', productsRouter)
 
 
+app.get('/api/products', (req,res)=>{
+    console.log('UPDATE PRODUCT')
+})
+
 const httpServer =  app.listen(8080, ()=>{
     console.log('Escuchando en el puerto 8080')
 })
@@ -36,14 +40,33 @@ const io = new ServerIO(httpServer)
 
 let mensajes = []
 
+
+async function updateJsonClient() {
+    try {
+        const response = await fetch('http://localhost:8080/api/products');
+        const jsonData = await response.json();
+        //console.log("RESPONSE \n\n\n\n\n" + JSON.stringify(jsonData.payload, null, 2))
+        io.emit('message', jsonData)
+        
+    } catch (error) {
+        console.error('Error al obtener datos JSON:', error);
+    }
+}
+
+
+setInterval(updateJsonClient, 300);
+
+
+
 function cbConnection(socket) {	
     console.log('cliente conectado')
-    // io.emit('')
     socket.on('message', data => {
         console.log(data)
         mensajes.push(data)
+        console.log('MENSAJE RECIBIDO EN EL SERVER')
+        updateJsonClient();
 
-        io.emit('messageLogs', mensajes)
+        
     })
 }
 
