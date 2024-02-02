e77f07200e9d254191f398a0f5ac415f0ddc46a7
arianaortigoza4 <arianaortigoza4@gmail.com>
2024-02-01 22:21:07 -0300

    Handlebars products

---
+++ Desafio6-practicaDeIntegracion/src/dao/models/users.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate-v2')
+
+
+const usersCollection = 'users'
+
+const usersSchema = new mongoose.Schema({
+    first_name: String,
+    last_name: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}) 
+usersSchema.plugin(mongoosePaginate)
+
+const userModel = mongoose.model(usersCollection, usersSchema)
+
+module.exports = {
+    userModel
+}
